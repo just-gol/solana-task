e5bf44066aadfbe329d964f7b7cbab82a049a638
aabb <809073023@qq.com>
2026-02-03 21:22:02 +0800

    Initial  blueshift_anchor_escrow

---
+++ blueshift_anchor_escrow/programs/blueshift_anchor_escrow/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6GNxX3PgZuHG3gi9o1gjye1CyyC3eVCTVGMQnV7EBnV7");
+
+#[program]
+pub mod blueshift_anchor_escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
